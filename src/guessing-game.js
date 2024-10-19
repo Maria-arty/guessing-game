939cefb7185c519ac147caad9952c1340778b274
MariaPetrovskaia <mariapet70@gmail.com>
2024-10-19 12:53:17 +0300

    feat:solve problem

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null
+        this.max = null
+        this.currentNumber = null
+    }
+    
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        this.currentNumber = Math.round((this.min + this.max) / 2)
+        return this.currentNumber
     }
 
     lower() {
-
+        this.max = this.currentNumber
     }
 
     greater() {
-
+        this.min = this.currentNumber
     }
 }
 
